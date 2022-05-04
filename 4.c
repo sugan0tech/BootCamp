751c3ba9593387c9256301444dab2ff6779bef45
sugan0tech <sugankpms@gmail.com>
2022-05-04 19:09:58 +0530

    updated 3rd one

---
+++ 4.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <math.h>
+int main()
+{
+    int hours;
+    printf("Enter the no of hours :");
+    scanf("%d", &hours);
+    if (hours >= 2 && hours < 3)
+        printf("Highly efficient");
+    else if (hours > 3 && hours < 4)
+        printf("Improve Speed");
+    else if (hours > 4 && hours < 5)
+        printf("Do training");
+    else
+        printf("leave the company");
+
+    return 0;
+}
--- Day 1 c programs/3.c
+++ Day 1 c programs/3.c
@@ -1,5 +1,58 @@
-#include <stdio.h>
-int main()
+void premium(char health, char gender, int age, char lives)
 {
-    return 0;
+    if (health == 'e')
+    {
+        // && (age>25&&age<35) && lives == "c" && gender="m"
+        if (age > 25 && age < 35)
+        {
+            if (lives == 'c')
+            {
+                if (gender == 'm')
+                {
+                    printf("the premium is Rs. 4 per thousand and his policy amount cannot exceed Rs. 2 lakhs.\n");
+                }
+                else if (gender == 'f')
+                {
+                    printf("the premium is Rs. 3 per thousand and her policy amount cannot exceed Rs. 1 lakh.\n");
+                }
+            }
+            else
+            {
+                printf("not insured\n");
+            }
+        }
+        else
+        {
+            printf("not insured\n");
+        }
+    }
+    else if (health == 'p')
+    {
+        if (age > 25 && age < 35)
+        {
+            if (lives == 'v')
+            {
+                if (gender == 'm')
+                {
+                    printf("the premium is Rs. 6 per thousand and his policy cannot exceed Rs. 10,000.\n");
+                }
+                else
+                {
+                    printf("not insured\n");
+                }
+            }
+            else
+            {
+                printf("not insured\n");
+            }
+        }
+        else
+        {
+            printf("not insured\n");
+        }
+    }
+    else
+    {
+        printf("not insured\n");
+    }
 }
